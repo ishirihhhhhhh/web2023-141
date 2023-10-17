4bb24e98829f4740b8242fdca5ed4a14f1704f83
ishirihhhhhhh <131091996+ishirihhhhhhh@users.noreply.github.com>
2023-10-17 10:07:15 +0900

    Update ex6-5.js

--- ex6-5.js
+++ ex6-5.js
@@ -2,5 +2,6 @@ var who65='world';function sayhello65(){ alert('Hello,'+who65+'!');}
 function taro65(){ who65='太郎';}
 function hanako65(){ who65='花子';}
 
+var who66='world';function sayhello66(){ alert('Hello,'+who66+'!');}
 function taro66(){ alert('太郎が来た,')}
 function hanako66(){ alert('花子が来た')}
