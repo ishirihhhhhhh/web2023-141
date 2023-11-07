9389d58477cc1a03b252a650acd3a9b587407b67
ishirihhhhhhh <131091996+ishirihhhhhhh@users.noreply.github.com>
2023-11-07 10:13:57 +0900

    Update ex6-10.js

--- ex6-10.js
+++ ex6-10.js
@@ -1,1 +1,2 @@
 function imgchange1(){document.getElementById('logo').src ='./image/image/png-transparent-josai-university-hd-logo.png';}
+function imgchange2(){document.getElementById('lodo').src= "}
