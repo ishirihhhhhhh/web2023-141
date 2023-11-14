5b426dda8784880c72e90b9d1e09cabb61e1a01e
ishirihhhhhhh <131091996+ishirihhhhhhh@users.noreply.github.com>
2023-11-14 10:22:34 +0900

    Update and rename ex6-12.js to ex6-11.js

---
+++ ex6-11.js
@@ -0,0 +1,2 @@
+function imgchange1(){document.getElementById('logo').src="image/download.png";}
+function imgchange2(){document.getElementById('logo').src="image/png-transparent-josai-university-hd-logo.png";}
--- ex6-12.js
+++
@@ -1,1 +0,0 @@
-function hyoji(){//あんぱん}
