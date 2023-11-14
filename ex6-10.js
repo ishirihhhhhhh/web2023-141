ee66fd938f2fab3c8d48a1f1d5039eae97996180
ishirihhhhhhh <131091996+ishirihhhhhhh@users.noreply.github.com>
2023-11-14 10:01:02 +0900

    Update ex6-10.js

--- ex6-10.js
+++ ex6-10.js
@@ -1,2 +1,2 @@
-function gazo23(){document.getElementById('ball').src ='./image/野球ボール.png';}
-function gazo22(){document.getElementById('ball').src ='./image/サッカーボール.png';}
+function gazo23(){document.getElementById('ball').src ='./image/サッカーボール.png';}
+function gazo22(){document.getElementById('ball').src ='./image/野球ボール.png';}
