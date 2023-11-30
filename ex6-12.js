60c2dcb656e03265bc1b22196c3d1a31ee3e73c0
ishirihhhhhhh <131091996+ishirihhhhhhh@users.noreply.github.com>
2023-11-30 15:32:17 +0900

    Update ex6-12.js

--- ex6-12.js
+++ ex6-12.js
@@ -7,12 +7,10 @@ var cnt =document.timer.moji.value.length;
 
 if(cnt　<　11　){
 //現在より1文字多く切り出して表示
-document.timer.moji.value=str.substr(0,cnt+1);
-}else{
+document.timer.moji.value=str.substr(0,cnt+1);}
+else{
 //すべて表示されたら、空文字に戻す
-ducument.timer.moji.value="";
-}
-}
+ducument.timer.moji.value="";}}
 function startfnc(){
 //関数hyoji()を1000ミリ秒間隔で呼び出す
 setInterval("hyoji()",1000);}
