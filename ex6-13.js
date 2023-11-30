633758c25006bc7aaacea9c83960818f51928c97
ishirihhhhhhh <131091996+ishirihhhhhhh@users.noreply.github.com>
2023-11-30 17:07:22 +0900

    Update ex6-13.js

--- ex6-13.js
+++ ex6-13.js
@@ -12,4 +12,4 @@ document.timer13.moji13.value = str13.substr(0,cnt13+1);}else{
 //全て表示されたら、空文字に戻す
 document.timer13.moji13.value="";}
 //setTimeout()を含む関数を呼び出す
-setTimeout("hyoji13()",1000;)}
+setTimeout("hyoji13()",1000);}
