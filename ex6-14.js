fbad7f55de7625946edb6d8ce61d66c18d8a7b77
ishirihhhhhhh <131091996+ishirihhhhhhh@users.noreply.github.com>
2023-12-02 16:05:17 +0900

    Update ex6-14.js

--- ex6-14.js
+++ ex6-14.js
@@ -1,5 +1,5 @@
 //function gazo33(){
-//<img src=”image/太陽.png" alt="gazohen3" class="imagechange3">
+//<img src=”image/太陽.png" alt="sun" class="imagechange3">
 //setTimeout("gazo33()",3000);}
 var img;
 function gazo33(){document.getElementById('img33').src=”image/月.png”; setTimeout("gazo34()",3000);}
