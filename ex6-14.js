ca849676e8fce703cbb570f621b8c7ef5cd65f79
ishirihhhhhhh <131091996+ishirihhhhhhh@users.noreply.github.com>
2023-12-02 15:44:27 +0900

    Create ex6-14.js

---
+++ ex6-14.js
@@ -0,0 +1,6 @@
+//function gazo33(){
+//<img src='' id="img33" alt="gazohen3" class="imagechange3">
+//setTimeout("gazo33()",3000);}
+var img;
+function gazo33(){document.getElementById('img33').src='./image/back_alley_3.jpg'; setTimeout("gazo34()",3000);}
+function gazo34(){document.getElementById('img33').src='./image/back_alley_2.jpg';}
