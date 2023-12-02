2de3539010f2efcf23a3dd5352e426780363be64
ishirihhhhhhh <131091996+ishirihhhhhhh@users.noreply.github.com>
2023-12-02 20:55:18 +0900

    Update ex6-14.js

--- ex6-14.js
+++ ex6-14.js
@@ -1,3 +1,3 @@
 var img;
-function imgchange3(){document.getElementById("wakusei"). src="./image/月/png"; setTimeout("imgchange4()",3000);}
-function imgchange4(){document.getElementById("wakusei"). src="./image/太陽.png";}
+function imgchange5(){document.getElementById("wakusei"). src="./image/月/png"; setTimeout("imgchange5()",3000);}
+function imgchange6(){document.getElementById("wakusei"). src="./image/太陽.png";}
