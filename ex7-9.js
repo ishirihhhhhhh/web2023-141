0cf3d08bb5cf7b06d1dde61f55d076f7fd87040f
ishirihhhhhhh <131091996+ishirihhhhhhh@users.noreply.github.com>
2023-12-12 10:08:47 +0900

    Update ex7-9.js

--- ex7-9.js
+++ ex7-9.js
@@ -1,1 +1,11 @@
-
+function nazo77(){
+if(document.getElementById('answer77'). value == '夢'){
+alert('あたり！');  
+}
+else if(document.getElementById('answer77'). value == 'ゆめ'){
+alert('あたり！');
+}
+else if{
+alert('はずれ！');
+}
+}
