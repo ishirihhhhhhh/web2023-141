b299c27c9a51493f154651e14cf3c20c4a0edd48
ishirihhhhhhh <131091996+ishirihhhhhhh@users.noreply.github.com>
2023-12-12 10:13:12 +0900

    Update ex7-9.js

--- ex7-9.js
+++ ex7-9.js
@@ -1,8 +1,8 @@
-function nazo77(){
-if(document.getElementById('answer77'). value == '夢'){
+function nazo99(){
+if(document.getElementById('answer99'). value == '夢'){
 alert('あたり！');  
 }
-else if(document.getElementById('answer77'). value == 'ゆめ'){
+else if(document.getElementById('answer99'). value == 'ゆめ'){
 alert('あたり！');
 }
 else{
